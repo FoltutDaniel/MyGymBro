11a16e0393622d806f05c652fef96c96c7d486ee
Daniel Foltut <danfoltut@gmail.com>
2023-05-20 15:11:16 +0300

    Refactor workout service

--- frontend/src/app/services/workout.service.ts
+++ frontend/src/app/services/workout.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Workout} from "../model/workout.model";
 import {Observable} from "rxjs";
 import {Exercise} from "../model/exercise.model";
+import {WorkoutExercise} from "../model/workout-exercise.model";
 
 @Injectable({
     providedIn: 'root'
@@ -16,12 +17,12 @@ export class WorkoutService {
         return this.httpClient.post<Workout>(this.url+'/start-workout', workout);
     }
 
-    addExerciseToWorkout(workout: Workout): Observable<Workout>{
-        return this.httpClient.put<Workout>(this.url+'/add-exercise', workout);
+    addExerciseToWorkout(workoutExercise: WorkoutExercise, workoutId: number): Observable<Workout>{
+        return this.httpClient.put<Workout>(this.url+'/add-exercise/' + workoutId, workoutExercise);
     }
 
-    removeExerciseToWorkout(workout: Workout): Observable<Workout>{
-        return this.httpClient.put<Workout>(this.url+'/remove-exercise', workout);
+    removeExerciseToWorkout(workoutExercise: WorkoutExercise, workoutId: number): Observable<Workout>{
+        return this.httpClient.put<Workout>(this.url+'/remove-exercise/' + workoutId, workoutExercise);
     }
 
     getAll(userId: number): Observable<Workout[]>{
